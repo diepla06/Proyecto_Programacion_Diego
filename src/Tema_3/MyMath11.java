53fff3ac5c035242efa6c000206fdb908138c172
diepla06 <diegoplana06@gmail.com>
2025-12-01 00:22:00 +0100

    MyMath

---
+++ src/Tema_3/MyMath11.java
@@ -0,0 +1,22 @@
+package Tema_3;
+
+import java.util.Scanner;
+
+public class MyMath7 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num;
+        boolean primo;
+
+        System.out.println("Dime un numero y te dire si es primo");
+        num = sc.nextInt();
+
+        primo = MyMath.esPrimo(num);
+
+        if (primo) {
+            System.out.println(num + " es primo");
+        }else  {
+            System.out.println(num + " no es primo");
+        }
+    }
+}
---
+++ src/Tema_3/MyMath8.java
@@ -0,0 +1,10 @@
+package Tema_3.array;
+
+import java.util.Scanner;
+
+public class MyMath8 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num;
+    }
+}
